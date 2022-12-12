8f910b771247f47f68505d75c6d296dbe6a365a6
MSHonore <mickael.saint-honore@chronopost.fr>
2022-12-12 17:21:57 +0100

    master : import flutter text to speech

---
+++ lib/core/texttospeech/texttospeechutils.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:flutter_tts/flutter_tts_web.dart';
+
+class TextToSpeechUtils {
+
+  FlutterTts? flutterTts;
+  late List<dynamic> languages;
+
+  FlutterTts getInstance() {
+    flutterTts ??= FlutterTts();
+    return flutterTts!;
+  }
+
+  void initTextToSpeech() async {
+    languages =  await flutterTts?.getLanguages;
+    await flutterTts?.setSharedInstance(true);
+    await flutterTts?.awaitSpeakCompletion(true);
+
+    await flutterTts?.setLanguage("fr-FR");
+
+    await flutterTts?.setSpeechRate(1.0);
+
+    await flutterTts?.setVolume(1.0);
+
+    await flutterTts?.setPitch(1.0);
+
+    await flutterTts?.isLanguageAvailable("en-US");
+
+    // iOS, Android and Web only
+    //see the "Pausing on Android" section for more info
+    await flutterTts?.pause();
+
+    // iOS, macOS, and Android only
+    await flutterTts?.synthesizeToFile("Hello World", Platform.isAndroid ? "tts.wav" : "tts.caf");
+
+    await flutterTts?.setVoice({"name": "Karen", "locale": "en-AU"});
+
+    // iOS only
+    await flutterTts?.setSharedInstance(true);
+
+    // Android only
+    await flutterTts?.setSilence(2);
+
+    await flutterTts?.getEngines;
+
+    await flutterTts?.getDefaultVoice;
+
+    await flutterTts?.isLanguageInstalled("en-AU");
+
+    await flutterTts?.areLanguagesInstalled(["en-AU", "en-US"]);
+
+    await flutterTts?.setQueueMode(1);
+
+    await flutterTts?.getMaxSpeechInputLength;
+  }
+}
--- lib/ui/library/library.dart
+++ lib/ui/library/library.dart
@@ -1,18 +1,154 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class LibraryWidget extends StatelessWidget {
+import 'package:csv/csv.dart';
+import 'package:ekang_flutter/core/texttospeech/texttospeechutils.dart';
+import 'package:ekang_flutter/generated/assets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:flutter_tts/flutter_tts_web.dart';
+
+class LibraryWidget extends StatefulWidget {
   const LibraryWidget({super.key});
+
+  @override
+  State<LibraryWidget> createState() => _LibraryState();
+}
+
+class _LibraryState extends State<LibraryWidget> {
+  List<List<dynamic>> _data = [];
+  late FlutterTts flutterTts;
+  TtsState? ttsState;
+  String? _currentWord;
+  String? textToSpeak;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (kDebugMode) {
+      log("initState()");
+    }
+
+    initTTS();
+    loadCsvFromAssets();
+  }
+
+  void loadCsvFromAssets() async {
+    if (kDebugMode) {
+      log("loadCsvFromAssets()");
+    }
+
+    final input = await rootBundle.loadString(Assets.csvTest);
+    final fields = const CsvToListConverter().convert(input);
+
+    if (null != fields) {
+      if (kDebugMode) {
+        log("fields : $fields");
+      }
+    }
+
+    setState(() {
+      _data = fields;
+    });
+  }
+
+  void initTTS() async {
+    flutterTts = TextToSpeechUtils().getInstance();
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setProgressHandler(
+        (String text, int startOffset, int endOffset, String word) {
+      setState(() {
+        _currentWord = word;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    // Android, iOS and Web
+    flutterTts.setPauseHandler(() {
+      setState(() {
+        ttsState = TtsState.paused;
+      });
+    });
+
+    flutterTts.setContinueHandler(() {
+      setState(() {
+        ttsState = TtsState.continued;
+      });
+    });
+
+    // Android only
+    flutterTts.setInitHandler(() {
+      setState(() {
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future _speak(String text) async {
+    var result = await flutterTts.speak(text);
+    if (result == 1) setState(() => ttsState = TtsState.playing);
+  }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Library route'),
-          onPressed: () {},
-        ),
-      ),
+      body: ListView.builder(
+          shrinkWrap: true,
+          itemCount: _data.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                textToSpeak = _data[index][1].toString();
+                log("onTap() | $textToSpeak");
+                _speak(textToSpeak!);
+              },
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('${_data[index][0]}'),
+                      Text(_data[index][1]),
+                      Text(_data[index][2]),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
     );
   }
 }
