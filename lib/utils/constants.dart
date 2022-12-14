0b898ea212c98c2c9b47fc703fbabf12f1d6c8bf
MichaelStH <michael.sainthonore@gmail.com>
2022-12-14 14:12:50 +0100

    master : add audio files in assets

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static const appName = 'Si Ekang';
+
+  static const homeTitle = 'Home';
+  static const libraryTitle = 'Library';
+  static const notificationsTitle = 'Notifications';
+  static const profileTitle = 'Profile';
+}
