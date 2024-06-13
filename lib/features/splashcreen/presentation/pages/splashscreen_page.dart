f19e9d93d6f87df03ee69250bd9b8ace3c7285a2
Mickael Saint-Honoré <mickael.saint-honore@chronopost.fr>
2024-06-13 15:47:10 +0200

    master : refactor splashscreen package

---
+++ lib/features/splashcreen/presentation/pages/splashscreen_page.dart
@@ -0,0 +1,8 @@
+import 'package:ekang_flutter/core/widgets/widgets.dart';
+
+class SplashScreenPage extends StatelessWidget {
+  const SplashScreenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => const SplashScreenPage();
+}
--- lib/features/splashcreen/splashscreen.dart
+++ lib/features/splashcreen/splashscreen.dart
@@ -1,3 +1,16 @@
+
+import 'package:ekang_flutter/features/home/presentation/widgets/home_widget.dart';
+import 'package:ekang_flutter/features/home/presentation/widgets/home_widget.dart';
+import 'package:ekang_flutter/features/library/presentation/pages/library_page.dart';
+import 'package:ekang_flutter/features/library/presentation/widgets/library_widget.dart';
+import 'package:ekang_flutter/features/main/presentation/bloc/main_bloc.dart';
+import 'package:ekang_flutter/features/notifications/presentation/widgets/notification_widget.dart';
+import 'package:ekang_flutter/features/profile/presentation/widgets/profile_widget.dart';
+
+
+
+
+import 'package:ekang_flutter/features/main/presentation/pages/main_page.dart';
 import 'package:ekang_flutter/features/main/presentation/widgets/main_widget.dart';
 import 'package:ekang_flutter/generated/assets.dart';
 import 'package:fimber/fimber.dart';
@@ -21,21 +34,11 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     Future.delayed(const Duration(milliseconds: 2500), () {
       // Here you can write your code for open new view
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const MainWidget()));
+          context, MaterialPageRoute(builder: (context) => const MainPage()));
 
       try {
         dispose();
