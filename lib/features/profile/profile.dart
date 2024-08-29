41c7739551813bd635408ce8007adfbf39cf9b03
Mickael Saint-Honoré <mickael.saint-honore@chronopost.fr>
2024-08-29 09:51:43 +0200

    master : refactor profile package

---
+++ lib/features/profile/profile.dart
@@ -0,0 +1,4 @@
+library profile;
+
+export 'package:ekang_flutter/features/profile/presentation/pages/profile_page.dart';
+export 'package:ekang_flutter/features/profile/presentation/widgets/profile_widget.dart';
