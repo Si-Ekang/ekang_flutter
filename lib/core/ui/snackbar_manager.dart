83587d505543200ea959369b3f2ca6480a6bfe09
Mickael Saint-Honoré <mickael.saint-honore@chronopost.fr>
2025-02-05 16:40:23 +0100

    master : improve widgets components

--- lib/core/ui/snackbar_manager.dart
+++ lib/core/ui/snackbar_manager.dart
@@ -1,29 +1,37 @@
 import 'package:ekang_flutter/core/widgets/widgets.dart';
 
 class SnackBarManager {
-  void showSuccessSnackBar(
-      {required String message, required BuildContext context}) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(
-          message,
-          style: const TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.green,
-      ),
-    );
+  void showSuccessSnackBar({
+    required BuildContext context,
+    required String message,
+  }) {
+    // Find the ScaffoldMessenger in the widget tree
+    // and use it to show a SnackBar.
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  message,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                backgroundColor: Colors.green,
+              ),
+            ));
   }
 
-  void showErrorSnackBar(
-      {required String message, required BuildContext context}) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(
-          message,
-          style: const TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.redAccent,
-      ),
-    );
+  void showErrorSnackBar({
+    required BuildContext context,
+    required String message,
+  }) {
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  message,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                backgroundColor: Colors.redAccent,
+              ),
+            ));
   }
 }
--- lib/core/widgets/widgets.dart
+++ lib/core/widgets/widgets.dart
@@ -9,3 +9,4 @@ export 'package:ekang_flutter/core/component/siekangtoolbar.dart';
 export 'package:ekang_flutter/core/theme/siekang_app_widget.dart';
 export 'package:ekang_flutter/core/theme/siekang_theme.dart';
 export 'package:ekang_flutter/core/theme/siekang_colors.dart';
+export 'package:ekang_flutter/core/theme/text_theme.dart';
