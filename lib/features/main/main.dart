c76746f8c48f0b907722b7b9aac44136e6debb8d
Mickael Saint-Honoré <mickael.saint-honore@chronopost.fr>
2025-02-05 14:17:50 +0100

    master : add main route variable

--- lib/features/main/main.dart
+++ lib/features/main/main.dart
@@ -1,2 +1,3 @@
+export 'package:ekang_flutter/features/main/presentation/bloc/main_bloc.dart';
 export 'package:ekang_flutter/features/main/presentation/pages/main_page.dart';
 export 'package:ekang_flutter/features/main/presentation/widgets/main_widget.dart';
--- lib/features/main/presentation/pages/main_page.dart
+++ lib/features/main/presentation/pages/main_page.dart
@@ -1,9 +1,10 @@
 import 'package:ekang_flutter/features/main/presentation/bloc/main_bloc.dart';
 import 'package:ekang_flutter/features/main/presentation/widgets/main_widget.dart';
-import 'package:flutter/material.dart';
+import 'package:ekang_flutter/core/widgets/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainPage extends StatelessWidget {
+  static const String routeName = "MainPage";
   const MainPage({super.key});
 
   // This widget is the root of your application.
