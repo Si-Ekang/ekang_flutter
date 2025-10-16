ef3a06f4d2121fc3dddbcf84b70ddc41deca2e64
MichaelStH <michael.sainthonore@gmail.com>
2025-10-16 15:29:17 +0200

    master : improve notification feature

---
+++ lib/features/notifications/presentation/bloc/notifications_event.dart
@@ -0,0 +1,11 @@
+part of 'notifications_bloc.dart';
+
+class NotificationsEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class NotificationsLoadImageEvent extends NotificationsEvent {
+  @override
+  List<Object?> get props => [];
+}
