ac71e6c38a6d2be2b7addc57a40d61cd7e53600d
MichaelStH <michael.sainthonore@gmail.com>
2025-11-10 15:20:58 +0100

    master : init mobile ads in MainActivity

--- android/app/src/main/kotlin/com/siekang/ekang_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/siekang/ekang_flutter/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.siekang.ekang_flutter
 
+import android.os.Bundle
+import com.google.android.gms.ads.MobileAds
 import io.flutter.embedding.android.FlutterActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            // Initialize the Google Mobile Ads SDK on a background thread.
+            MobileAds.initialize(this@MainActivity) {}
+        }
+    }
 }
